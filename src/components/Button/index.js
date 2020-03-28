eaa97b3dda2c8bc474e2b0c437bd0ed5a0f0d34e
Anoop <anooprav7@gmail.com>
2020-03-28 23:53:11 +0530

    added theme colors + emotion js

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+/**
+ * Props
+ *  ...spread
+ *  appearance = primary, secondary,
+ *  XXX variant = capsule
+ *  outline
+ *  block
+ *  disabled
+ *  loading
+ *  href - anchor
+ *  size = small, medium, large
+ *
+ */
