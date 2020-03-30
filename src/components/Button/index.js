527d7cda91900e1a8848330874b4d7f4e7f1b057
Nabeelt <nabeel.thurakkal@cleartrip.com>
2020-03-30 21:11:29 +0530

    Typography props table added

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "@emotion/styled"
 import PropTypes from "prop-types"
-import theme from "../../theme"
+// import theme from "../../theme"
 
 import styles from "./styles"
 
